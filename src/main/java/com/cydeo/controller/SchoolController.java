168f5ee6e0e01a41069b2464a654adbf5012a952
ftmdrnclv <duranf64@hotmail.com>
2024-01-01 21:27:45 +0100

    controller is created

---
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -0,0 +1,96 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.AddressService;
+import com.cydeo.service.ParentService;
+import com.cydeo.service.StudentService;
+import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SchoolController {
+
+    //write a method for teachers and return list of teachers
+
+    private final TeacherService teacherService;
+    private final StudentService studentService;
+    private final ParentService parentService;
+    private final AddressService addressService;
+
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
+        this.teacherService = teacherService;
+        this.studentService = studentService;
+        this.parentService = parentService;
+        this.addressService = addressService;
+    }
+
+    @GetMapping("/teachers")
+    public List<TeacherDTO> allTeachers(){
+
+        return teacherService.findAll();
+    }
+    /*  7MIN
+        create an endpoint for students, where json response includes
+        "students are successfully retrieved." message
+        code:200
+        success:true
+        and student data
+     */
+
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> readAllStudents(){
+
+        return ResponseEntity.ok(new ResponseWrapper
+                ("students are successfully retrieved."
+                ,studentService.findAll()));
+    }
+
+    /*     7 MIN
+           create a parents endpoint where status code is 202
+           additional header has "Parent" , "Returned"
+           and following body structure
+           "parents are successfully retrieved." message
+           code:202
+           success: true
+           and parent data.
+     */
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+
+        return ResponseEntity
+                            .status(HttpStatus.ACCEPTED)
+                            .header("Parent","Returned")
+                            .body(new ResponseWrapper(true,"parents are successfully retrieved.",
+                                    HttpStatus.ACCEPTED.value(),parentService.findAll()));
+    }
+
+    /*  6MIN
+        create an endpoint for individual address information
+        /address/1 2 3
+        return status code 200
+        "address .. is successfully retrieved" message
+        success true
+        and address information
+     */
+    @GetMapping("/address/{id}")
+    public ResponseEntity<ResponseWrapper> getAddressById(@PathVariable("id") Long id) throws Exception {
+
+        //find the address to return
+        AddressDTO addressDTO = addressService.findById(id);
+
+        return ResponseEntity.ok(new ResponseWrapper("Address "+id+" is successfully retrieved",addressDTO));
+
+    }
+
+
+
+}
